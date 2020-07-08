80718c2a07a442cb5095c091c00a1d678876fe48
Richard Wall <richard.wall@jetstack.io>
2020-07-08 16:36:22 +0100

    Use kubeadm alpha certs generate-csr in demo
    Signed-off-by: Richard Wall <richard.wall@jetstack.io>

--- docs/demos/kubeadm-bootstrap/kubeadm-bootstrap.sh
+++ docs/demos/kubeadm-bootstrap/kubeadm-bootstrap.sh
@@ -14,6 +14,7 @@ KUBERNETES_DIR="${WORK_DIR}/etc_kubernetes"
 CERTIFICATES_DIR="${KUBERNETES_DIR}/pki"
 VCERT_ZONE="TLS/SSL\Certificates\Kubernetes\Cluster1"
 VCERT_CA="${SCRIPT_DIR}/ca.venafi.crt"
+KUBEADM_CONF="${SCRIPT_DIR}/kubeadm.conf"
 
 : ${VCERT_INI:?}
 : ${KIND:?}
@@ -24,29 +25,20 @@ function log() {
     echo "# $(date --rfc-3339=ns) :: ${*}" >&2
 }
 
-function kubeadm_init_phase_certs() {
-    while read cert; do
-        log "Generating CSR ${cert}"
-        local args=""
-        if [[ "${cert}" == "apiserver" ]]; then
-            args="--apiserver-cert-extra-sans=kind-control-plane,127.0.0.1"
-        fi
-        ${KUBEADM} init phase certs "${cert}" ${args} --csr-only --csr-dir "${CERTIFICATES_DIR}" --cert-dir "${CERTIFICATES_DIR}" >/dev/null
-        echo "${cert} ${CERTIFICATES_DIR}/${cert/#etcd-/etcd\/}.csr"
-    done
-}
-
 function vcert_enroll() {
     while read nickname csr; do
         cert="${csr/%.csr/.crt}"
-        policy="$(echo "${nickname}" | grep -o '\(client\|server\|peer\)$')"
+        policy="$(echo "${nickname}" | grep -o '\(client\|server\|peer\)$' || true)"
         # Special case for etcd-server which needs both server and client usage
         # See https://clusterise.com/articles/kbp-2-certificates/ and
         # https://kubernetes.io/docs/setup/best-practices/certificates/#all-certificates
         if [[ "${nickname}" == "etcd-server" ]]; then
             policy=peer
         fi
-        log "Enrolling CSR ${csr} with nickname ${nickname} to ${cert}"
+        if echo "${nickname}" | grep '\.conf$' > /dev/null; then
+            policy=client
+        fi
+        log "Enrolling CSR ${csr} with nickname ${nickname} to ${cert} with policy ${policy}"
         vcert enroll -z "${VCERT_ZONE}\\${policy}"  --config ${VCERT_INI} --nickname "${nickname}" --csr "file:${csr}" --cert-file "${cert}" >/dev/null
     done
 }
@@ -64,39 +56,35 @@ log "Creating certificates directory ${CERTIFICATES_DIR}"
 mkdir -p "${CERTIFICATES_DIR}"
 
 log "Generating certificate signing requests"
-kubeadm_init_phase_certs <<EOF | vcert_enroll
-  apiserver
-  apiserver-etcd-client
-  apiserver-kubelet-client
-  etcd-healthcheck-client
-  etcd-peer
-  etcd-server
-  front-proxy-client
-EOF
+${KUBEADM} alpha certs generate-csr \
+           --config ${KUBEADM_CONF} \
+           --cert-dir ${CERTIFICATES_DIR} \
+           --kubeconfig-dir ${KUBERNETES_DIR}
 
-log "Creating self-signed certificate authority"
-${KUBEADM} init phase certs ca --cert-dir "${PWD}/pki.self-signed" >/dev/null
-
-log "Creating Kubeconfigs"
-${KUBEADM} init phase kubeconfig all \
-           --cert-dir "${PWD}/pki.self-signed" \
-           --kubeconfig-dir "${KUBERNETES_DIR}" \
-           --control-plane-endpoint kind-control-plane \
-           --node-name kind-control-plane
+find ${KUBERNETES_DIR} -name '*.csr' | \
+    while read path; do
+        nickname=$(basename $path .csr)
+        if echo $path | fgrep '/etcd/' >/dev/null; then
+            nickname="etcd-${nickname}"
+        fi
+        echo $nickname $path
+    done | \
+        vcert_enroll
 
 log "Setting Venafi CA in all kubeconfigs"
 venafi_ca_data=$(base64 -w 0 < "${SCRIPT_DIR}/ca.venafi.crt")
 find "${KUBERNETES_DIR}" -name '*.conf'  | \
-    xargs -n 1 -I {} -- \
-          kubectl --kubeconfig={} config set clusters.kubernetes.certificate-authority-data "${venafi_ca_data}"
+    while read path; do
+        kubectl --kubeconfig=${path} config set clusters.kubernetes.certificate-authority-data "${venafi_ca_data}"
+        context=$(kubectl --kubeconfig "${path}" config current-context)
+        kubectl --kubeconfig=${path} config set users.${context%%@kubernetes}.client-certificate-data "$(base64 -w 0 < "${path}.crt")"
+    done
 
 log "Creating SA"
 ${KUBEADM} init phase certs sa --cert-dir "${CERTIFICATES_DIR}" >/dev/null
 
 log "Installing Venafi CA cert"
-# venafi cert must come first
-# TODO: Why?
-cat "${VCERT_CA}" "${PWD}/pki.self-signed/ca.crt" > "${CERTIFICATES_DIR}/ca.crt"
+cp "${VCERT_CA}" "${CERTIFICATES_DIR}/ca.crt"
 cp "${VCERT_CA}" "${CERTIFICATES_DIR}/front-proxy-ca.crt"
 cp "${VCERT_CA}" "${CERTIFICATES_DIR}/etcd/ca.crt"
 
